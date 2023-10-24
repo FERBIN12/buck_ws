084a4e1860d4dd284cb9e87b80e14b40fd4c3da8
FERBIN12 <fjferbin@gmail.com>
2023-10-24 19:02:18 +0530

    tested_the_bot_in_gazebo

--- install/model_py/share/model_py/launch/test.launch.py
+++ install/model_py/share/model_py/launch/test.launch.py
@@ -1,24 +1,42 @@
 import os
-from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import ExecuteProcess
+from launch.actions import DeclareLaunchArgument, ExecuteProcess, IncludeLaunchDescription
+from launch.conditions import IfCondition, UnlessCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import Command, LaunchConfiguration, PythonExpression
 from launch_ros.actions import Node
-
+from launch_ros.substitutions import FindPackageShare
+from ament_index_python.packages import get_package_prefix
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
+import xacro
 def generate_launch_description():
-    package_directory = get_package_share_directory('model_py')
-    world_file = os.path.join(package_directory, 'world', 'empty.world')
-    urdf_file = os.path.join(package_directory, 'urdf', 'run.urdf.xacro')
+    pkg_name = 'model_py'
+    file_subpath = 'urdf/run.urdf.xacro'
 
-    return LaunchDescription([
-        ExecuteProcess(
-            cmd=['gazebo', '--verbose', world_file, '-s', 'libgazebo_ros_factory.so'],
-            output='screen'
-        ),
-        Node(
-            package='robot_state_publisher',
-            executable='robot_state_publisher',
-            name='robot_state_publisher',
-            output='screen',
-            parameters=[{'robot_description': urdf_file}]
+    xacro_file = os.path.join(get_package_share_directory(pkg_name),file_subpath)
+    robot_description_raw = xacro.process_file(xacro_file).toxml()
+
+    node_robot_state_publisher = Node(
+        package = 'robot_state_publisher',
+        executable = 'robot_state_publisher',
+        output = 'screen',
+        parameters=[{'robot_description': robot_description_raw, 'use_sim_time':True}]
+
+    )
+
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('gazebo_ros'), 'launch'), '/gazebo.launch.py']),
         )
+    
+    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
+                    arguments=['-topic', 'robot_description',
+                                '-entity', 'my_bot'],
+                    output='screen')
+    
+    return LaunchDescription([
+        gazebo,
+        node_robot_state_publisher,
+        spawn_entity
     ])
